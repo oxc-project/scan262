4483edf23f81f98f5076f65d28034dddaddd799f
Boshen <boshenc@gmail.com>
2024-12-18 22:10:24 +0800

    logical assignment

--- src/implementation/es2021_logical_assignment.rs
+++ src/implementation/es2021_logical_assignment.rs
@@ -1,6 +1,15 @@
-use oxc::semantic::{AstNode, Semantic};
+use oxc::{
+    ast::AstKind,
+    semantic::{AstNode, Semantic},
+};
 
 use crate::{ctx::Ctx, feature::Feature, features::Es2021LogicalAssignment};
 impl Feature for Es2021LogicalAssignment {
-    fn test(&self, _node: &AstNode<'_>, _semantic: &Semantic<'_>, _ctx: &mut Ctx) {}
+    fn test(&self, node: &AstNode<'_>, _semantic: &Semantic<'_>, ctx: &mut Ctx) {
+        let AstKind::AssignmentExpression(expr) = node.kind() else { return };
+        if !expr.operator.is_logical() {
+            return;
+        }
+        ctx.diagnostic(self, expr.span);
+    }
 }
