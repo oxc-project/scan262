a4248be56019f4a9b9e363690ad3d89525f6017e
Boshen <boshenc@gmail.com>
2024-12-18 22:01:19 +0800

    nullish coalescing operator

--- src/implementation/es2020_nullish_coalescing_operator.rs
+++ src/implementation/es2020_nullish_coalescing_operator.rs
@@ -1,6 +1,16 @@
 use crate::features::Es2020NullishCoalescingOperator;
 use crate::{ctx::Ctx, feature::Feature};
-use oxc::semantic::{AstNode, Semantic};
+use oxc::ast::ast::LogicalOperator;
+use oxc::{
+    ast::AstKind,
+    semantic::{AstNode, Semantic},
+};
 impl Feature for Es2020NullishCoalescingOperator {
-    fn test(&self, _node: &AstNode<'_>, _semantic: &Semantic<'_>, _ctx: &mut Ctx) {}
+    fn test(&self, node: &AstNode<'_>, _semantic: &Semantic<'_>, ctx: &mut Ctx) {
+        let AstKind::LogicalExpression(expr) = node.kind() else { return };
+        if expr.operator != LogicalOperator::Coalesce {
+            return;
+        }
+        ctx.diagnostic(self, expr.span);
+    }
 }
